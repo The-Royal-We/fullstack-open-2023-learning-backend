7c5d0dea6b4cd7da0e12661db4af6101c79362ba
Brendan Carroll <brendan.carroll@workhuman.com>
2023-04-06 18:30:52 +0100

    complete exercise 3.3

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.get("/api/persons", (_request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
 app.get("/info", (_request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length} people </p><br/>${new Date()}`
